268d0c409d762ee78ba5583a80d40682ef2dbdff
Ramon Marcu <ramondotmarcu@gmail.com>
2020-12-18 23:39:57 +0200

    src and instructions: Codecademy

--- Kelvin Weather.js
+++ Kelvin Weather.js
@@ -5,15 +5,10 @@ const celsius = kelvin - 273;
 let fahrenheit = celsius * (9/5) + 32;
 // round down forenheit variable.
 fahrenheit = Math.floor(fahrenheit);
-console.log(`The temperature ${fahrenheit} degrees fahrenheit.`);
+console.log(`The temperature is ${fahrenheit} degrees fahrenheit.`);
 let newton = celsius * (33/100);
 newton = Math.floor(newton);
 console.log(`The temperature is ${newton} degrees newton.`);
 
 
-
-
-
-
-// if run, the code block will log to the console: The temperature -460 degrees fahrenheit.
-The temperature is -91 degrees newton.
+// if run the code block, the result will be: The temperature is -460 degrees fahrenheit. The temperature is -91 degrees newton.
